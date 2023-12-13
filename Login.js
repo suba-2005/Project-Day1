d98c2d9d5b594f48846899794f6a0630d413471f
suba-2005 <118423605+suba-2005@users.noreply.github.com>
2023-12-13 11:57:12 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,24 @@
+import React from "react";
+const Login=()=>{
+    return(
+        <div className="wrapper">
+            <form action="">
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Username" required/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required/>
+                </div>
+                <div className="remember-forgot">
+                    <a>Forgot Password?</a>
+                </div>
+                <button type="submit">Login</button>
+                <div className="register-link">
+                    <p>Or  <br/><br/><a href='#'>Signup</a></p>
+                </div>
+            </form>
+         </div>
+    );
+}
+export default Login;
